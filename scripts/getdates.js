cdf758a37a03c6289dd533ced4990c215b2b4b06
Israel <hancellsud@gmail.com>
2025-11-08 20:38:56 -0500

    code week02

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-  const yearSpan = document.getElementById("currentYear");
-  const lastModifiedSpan = document.getElementById("lastModified");
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
 
 
-  yearSpan.textContent = new Date().getFullYear();
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
 
- 
-  const lastModified = new Date(document.lastModified);
-  lastModifiedSpan.textContent = `Última modificación: ${lastModified.toLocaleString('es-EC')}`;
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modification: ${lastModified}`;
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  menuButton.textContent = menuButton.textContent === "☰" ? "✖" : "☰";
 });
+
