8b3ba64e3540f1048d8f274a171da07435d59489
Israel <hancellsud@gmail.com>
2025-11-27 22:33:38 -0500

    more changes week05

--- scripts/review.js
+++ scripts/review.js
@@ -27,3 +27,13 @@ const counterDisplay = document.querySelector("#reviewCount");
     localStorage.setItem("reviewCount", count);
     counterDisplay.textContent = count; 
  }
+// current year
+ document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+
+const menuButton = document.getElementById("menu-button");
+const navMenu = document.getElementById("nav-menu");
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modification: ${lastModified}`;
